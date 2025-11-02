49dd91809ab871e8a4eb9a02567746921fb4b11a
Muhammad Afif <afifpippop@gmail.com>
2025-11-02 15:53:45 +0700

    fix: 🐛 application form order

--- lib/application_form/application-form-builder.ts
+++ lib/application_form/application-form-builder.ts
@@ -117,14 +117,14 @@ type ApplicationFormBuilderSchema = z.infer<
 
 const APPLICATION_FORM_FIELDS_BUILDER_DEFAULT_VALUE: ApplicationFormBuilderSchema =
   {
-    full_name: APPLICATION_FORM_FIELD_OPTIONS.Mandatory,
     photo_profile: APPLICATION_FORM_FIELD_OPTIONS.Mandatory,
-    gender: APPLICATION_FORM_FIELD_OPTIONS.Mandatory,
-    domicile: APPLICATION_FORM_FIELD_OPTIONS.Mandatory,
+    full_name: APPLICATION_FORM_FIELD_OPTIONS.Mandatory,
     email: APPLICATION_FORM_FIELD_OPTIONS.Mandatory,
+    gender: APPLICATION_FORM_FIELD_OPTIONS.Mandatory,
+    date_of_birth: APPLICATION_FORM_FIELD_OPTIONS.Mandatory,
+    domicile: APPLICATION_FORM_FIELD_OPTIONS.Mandatory,
     phone_number: APPLICATION_FORM_FIELD_OPTIONS.Mandatory,
     linkedin_link: APPLICATION_FORM_FIELD_OPTIONS.Mandatory,
-    date_of_birth: APPLICATION_FORM_FIELD_OPTIONS.Mandatory,
   } as const;
 
 export {
